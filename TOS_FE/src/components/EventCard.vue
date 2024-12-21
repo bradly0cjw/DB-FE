8b2c21341f869d97e2a0eb31d4653e18812ef141
ice477 <ice.shadow477@gmail.com>
2024-12-21 16:33:10 +0800

    Home&Event

---
+++ TOS_FE/src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card bg-secondary text-white">
+      <img :src="product.image" class="card-img-top" :alt="product.name" />
+      <div class="card-body">
+        <h5 class="card-title">{{ product.name }}</h5>
+        <p class="card-text">{{ product.description }}</p>
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProductCard',
+    props: {
+      product: Object,
+    },
+  };
+  </script>
+  
---
+++ TOS_FE/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center">Hottest Events!!!</h1>
+    <div class="row mt-4">
+      <div
+        class="col-md-4"
+        v-for="product in products"
+        :key="product.id"
+      >
+        <EventCard :product="product" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import products from "@/data/test.json";
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      products,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  padding-top: 20px;
+}
+</style>
